4aaccc90953eeb52cadb154d185d029f614f6833
shukh <@shuxrat-xolmamatov-230139>
2023-11-14 22:40:37 +0500

    q

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -14,33 +14,33 @@ std::string problemSolution4(const std::string &macAddress) {
 //        result = "multicast";
 //    }
 //    return result;
-    std::string macAddressX = macAddress.substr(0,2);
-    std::string hexadecimal_letters[6] = {"A","B","C","D","E","F"};
-    std::string hexadecimal_integers[6] = {"10","11","12","13","14","15"};
+    std::string hex_ints[6] = {"10","11","12","13","14","15"};
+    std::string hex_letters[6] = {"A","B","C","D","E","F"};
+    std::string macAddress_subs = macAddress.substr(0,2);
     std::string num;
     std::string result;
-    int temp = 0;
+    int j = 0;
 
     for (int count=1; count != -1; count--){
-        num = macAddressX[count];
-        if (int(num[0])>=65 & int(num[0]<=70) ){
+        num = macAddress_subs[count];
+        if (int(num[0])>=65 && int(num[0]<=70) ){
             for (int i=0; i < 7; i++){
-                if (hexadecimal_letters[i] == num){
-                    num = hexadecimal_integers[i];
+                if (hex_letters[i] == num){
+                    num = hex_ints[i];
                     break;
                 };
             };
         };
-        temp = temp + (std::stoi(num))*pow(16, 2-count-1);
+        j = j + (std::stoi(num))*pow(16, 2-count-1);
     }
 
-    if (temp == 255){
-        result = "Broadcast";
+    if (j == 255){
+        result = "broadcast";
     } else {
-        if (temp%2 == 1){
-            result = "Multicast";
+        if (j%2 == 1){
+            result = "multicast";
         } else{
-            result = "Unicast";
+            result = "unicast";
         }
     }
 
