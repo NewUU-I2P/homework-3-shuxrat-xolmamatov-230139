56712d398cb0b739a1238ccd6a28804b0aee5715
shukh <@shuxrat-xolmamatov-230139>
2023-11-14 23:06:32 +0500

    qq

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,7 +1,8 @@
 
 
 float problemSolution1(float consumed_water) {
-    float cost = 0;
+    float cost;
+    float fixed_amount = 13;
     // write your code here
 //    if(consumed_water >=60){
 //        cost = 13 + 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.4;
@@ -14,13 +15,13 @@ float problemSolution1(float consumed_water) {
 //    }
 
     if(consumed_water <= 30){
-        cost = 13 + (30 - consumed_water);
-    }else if(consumed_water > 30 && consumed_water < 50){
-        cost = 13 + 30 * 0.4 + (consumed_water - 30) * 0.12 ;
-    }else if(consumed_water >= 50 && consumed_water < 60){
-        cost = 13 + 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
+        cost = fixed_amount + (30 - consumed_water);
+    }else if(consumed_water <= 50){
+        cost = fixed_amount + 30 * 0.4 + (consumed_water - 30) * 0.12 ;
+    }else if(consumed_water <= 60){
+        cost = fixed_amount + 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
     }else{
-        cost = 13 + 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.4;
+        cost = fixed_amount + 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.4;
     }
 
 
