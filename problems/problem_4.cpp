dea4d2c8f4e2d1e5a9f0020b1e0a2c4b7be44107
shukh <@shuxrat-xolmamatov-230139>
2023-11-14 22:22:57 +0500

    qq

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,15 +3,16 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
+    std::string result;
     int ans = 0;
     if(macAddress == "FF:FF:FF:FF:FF:FF"){
-        return  "broadcast";
+        result =   "broadcast";
     }else if(macAddress[1]%2 == 0){
-        return "uni cast";
+        result = "uni cast";
     }else{
-        return "multicast";
+        result = "multicast";
     }
-
+    return result;
     // make use of control flow statements
     // return result;
 }
