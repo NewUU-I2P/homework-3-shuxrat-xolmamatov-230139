eb4e9b6e78162f1004fd8ad8bca04d1dba313d24
shukh <@shuxrat-xolmamatov-230139>
2023-11-14 21:40:49 +0500

    Problem test

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,7 +1,30 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
-   if(x + y)
+    switch (operation) {
+        case '+':
+            result = x + y;
+            return result;
+            break;
+        case '-':
+            result = x - y;
+            return result;
+            break;
+        case '*':
+            result = x * y;
+            return result;
+            break;
+        case '/':
+            if (y != 0) {
+                result = x / y;
+                return result;
+            } else {
+                return "Error: Division by zero is not allowed.";
+            }
+            break;
+        default:
+            return "Error: Invalid arithmetic operation symbol.";
+    }
 
    return result;
 }
