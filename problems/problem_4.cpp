3acc8348f1bfc9458fb24e02f050278fab09b3cf
shukh <@shuxrat-xolmamatov-230139>
2023-11-14 21:28:32 +0500

    Problem test

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -1,8 +1,16 @@
-#include <string>
+﻿#include <string>
 #include <sstream>
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
+    int ans = 0;
+    if(macAddress == "FF:FF:FF:FF:FF:FF"){
+        return "broadcast";
+    }else if(macAddress[1]%2 == 0){
+        return "unicast";
+    }else{
+        return "multicast";
+    }
 
     // make use of control flow statements
     // return result;
