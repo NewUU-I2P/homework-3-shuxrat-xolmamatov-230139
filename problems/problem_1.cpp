2449cb80b8d0d3da87ba208acb9a7000d3362856
shukh <@shuxrat-xolmamatov-230139>
2023-11-14 23:14:56 +0500

    qq

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -13,9 +13,9 @@ float problemSolution1(float consumed_water) {
 //    }
 
     if(consumed_water <= 30){
-        cost = fixed_amount + (30 - consumed_water);
+        cost = fixed_amount + consumed_water * 0.4;
     }else if(consumed_water <= 50){
-        cost = fixed_amount + (30 * 0.4) + (consumed_water - 30) * 0.12 ;
+        cost = fixed_amount + (30 * 0.4) + (consumed_water - 30) * 0.12;
     }else if(consumed_water <= 60){
         cost = fixed_amount + (30 * 0.4) + (20 * 0.12)+ (consumed_water - 50) * 1.4;
     }else{
